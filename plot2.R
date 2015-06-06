7173e01bc999c64533d5fb2fd9646a873c7a62be
Bucky23 <lscf70@aol.com>
2015-06-06 08:54:38 -0500

    Course Project 1

--- plot2.R
+++ plot2.R
@@ -4,8 +4,9 @@ hhPower <- hhPower_full[(hhPower_full$Date =="2007-02-01") | (hhPower_full$Date 
 rm(hhPower_full)
 
 hhPower$Global_active_power <- as.numeric(as.character(hhPower$Global_active_power))
+hhPower$datetime <- as.POSIXct(paste(hhPower$Date, hhPower$Time))
 
-hist(hhPower$Global_active_power, xlab="Global Active Power (kilowatts)",col="red",main="Global Active Power")
+with(hhPower, plot(datetime,Global_active_power,type="l",xlab="", ylab="Global Active Power (kilowatts)"))
 
-dev.copy(png, file="plot1.png")
+dev.copy(png, file="plot2.png")
 dev.off()
